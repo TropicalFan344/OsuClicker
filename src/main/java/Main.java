60029083059dc681e1e0a864e82d7271ba926109
TropicalFan344 <1080101@gm.fdhs.tyc.edu.tw>
2021-12-26 00:43:08 +0800

    Fised

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,23 +17,41 @@ public class Main implements NativeKeyListener{
         int bpm;
         System.out.print("Please enter the BPM\n>> ");
         bpm = scanner.nextInt();
-        float spb = (60f/bpm)*1000;
+        float spb = (15f/bpm)*1000;
+        System.out.println(spb);
         new Thread(() -> {
             long lastTime = 0;
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(lastTime);
             while (true) {
+                System.out.print("");
                 if (isPressed) {
-                    if (System.currentTimeMillis() - lastTime > spb || true){
+                    if (System.currentTimeMillis() - lastTime > spb-1){
+                        lastTime = System.currentTimeMillis();
                         if (pressedA[0] = !pressedA[0]) {
+                            System.out.println("pressed A!");
                             robot.keyPress(KeyEvent.VK_A);
+                            try {
+                                Thread.sleep(5);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
                             robot.keyRelease(KeyEvent.VK_A);
 
                         }else {
+                            System.out.println("pressed X!");
                             robot.keyPress(KeyEvent.VK_X);
+                            try {
+                                Thread.sleep(5);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
                             robot.keyRelease(KeyEvent.VK_X);
                         }
-                        lastTime = System.currentTimeMillis();
-                    }else{
-                        System.out.println(System.currentTimeMillis() - lastTime + ", " + spb);
                     }
                 }
             }
@@ -57,18 +75,17 @@ public class Main implements NativeKeyListener{
 
     @Override
     public void nativeKeyPressed(NativeKeyEvent nativeKeyEvent) {
-        System.out.println();
         if (NativeKeyEvent.getKeyText(nativeKeyEvent.getKeyCode()).equalsIgnoreCase("d")) {
-            System.out.println("Pressed!");
             isPressed = true;
+            System.out.println("Pressed!" + isPressed);
         }
     }
 
     @Override
     public void nativeKeyReleased(NativeKeyEvent nativeKeyEvent) {
         if (NativeKeyEvent.getKeyText(nativeKeyEvent.getKeyCode()).equalsIgnoreCase("d")) {
-            System.out.println("Realeased!");
             isPressed = false;
+            System.out.println("Realeased!" + isPressed);
         }
 
     }
