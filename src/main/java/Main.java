8dbc7efaedffa75679c2eeee9d68eb49460d57aa
TropicalFan344 <1080101@fdhs.gm.tyc.edu.tw>
2022-02-13 05:49:46 +0800

    removed debug

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -26,14 +26,12 @@ public class Main implements NativeKeyListener{
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            System.out.println(lastTime);
             while (true) {
                 System.out.print("");
                 if (isPressed) {
                     if (System.currentTimeMillis() - lastTime > spb-1){
                         lastTime = System.currentTimeMillis();
                         if (pressedA[0] = !pressedA[0]) {
-                            System.out.println("pressed A!");
                             robot.keyPress(KeyEvent.VK_A);
                             try {
                                 Thread.sleep(5);
@@ -43,7 +41,6 @@ public class Main implements NativeKeyListener{
                             robot.keyRelease(KeyEvent.VK_A);
 
                         }else {
-                            System.out.println("pressed X!");
                             robot.keyPress(KeyEvent.VK_X);
                             try {
                                 Thread.sleep(5);
@@ -75,9 +72,9 @@ public class Main implements NativeKeyListener{
 
     @Override
     public void nativeKeyPressed(NativeKeyEvent nativeKeyEvent) {
+        System.out.println(NativeKeyEvent.getKeyText(nativeKeyEvent.getKeyCode()));
         if (NativeKeyEvent.getKeyText(nativeKeyEvent.getKeyCode()).equalsIgnoreCase("d")) {
             isPressed = true;
-            System.out.println("Pressed!" + isPressed);
         }
     }
 
@@ -85,7 +82,6 @@ public class Main implements NativeKeyListener{
     public void nativeKeyReleased(NativeKeyEvent nativeKeyEvent) {
         if (NativeKeyEvent.getKeyText(nativeKeyEvent.getKeyCode()).equalsIgnoreCase("d")) {
             isPressed = false;
-            System.out.println("Realeased!" + isPressed);
         }
 
     }
